291e61ad772d26890bb71af98fff0a9d80f16f8f
OxoGhost <arlenghost@proton.me>
2025-11-21 22:50:25 +0100

    adapted to fit the changes on game_instance_manager.py

--- training_launcher.py
+++ training_launcher.py
@@ -5,6 +5,7 @@ from TMI.game_instance_manager import GameInstanceManager
 from agent.policy_adapter import make_exploration_policy
 import numpy as np
 
+
 def update_network():
     pass  # Will call PPOTrainer.update() later cuz now i'm checking if this works
 
@@ -15,8 +16,8 @@ if __name__ == "__main__":
     gim = GameInstanceManager(game_spawning_lock=None, tmi_port=5400)
     results, stats = gim.rollout(
         exploration_policy=exploration_policy,
-        map_path="A01-Race.Challenge.Gbx",
-        zone_centers=np.zeros((10, 3)),  # dummy placeholder
+        map_path="Level-1.Challenge.Gbx",
+        zone_centers_filename="level1_0.5m_cl.npy",
         update_network=update_network
     )
 
